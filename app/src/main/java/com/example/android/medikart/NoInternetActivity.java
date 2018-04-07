c8b8b0a04df0e0dd97674516d8d6dc674e706c83
harsh-2711 <32356267+harsh-2711@users.noreply.github.com>
2018-04-07 11:30:42 +0530

    No Internet Activity added along with Splash Screen activity

--- app/src/main/java/com/example/android/medikart/NoInternetActivity.java
+++ app/src/main/java/com/example/android/medikart/NoInternetActivity.java
@@ -7,22 +7,30 @@ import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.android.medikart.R;
-
-public class MainActivity extends AppCompatActivity {
+public class NoInternetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_no_internet);
     }
-
     protected void onStart(){
         super.onStart();
 
-        if(!isNetworkAvailable())
+        if(isNetworkAvailable())
         {
-            Intent i = new Intent(MainActivity.this, NoInternetActivity.class);
+            Intent i = new Intent(NoInternetActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+
+        if(isNetworkAvailable())
+        {
+            Intent i = new Intent(NoInternetActivity.this, MainActivity.class);
             startActivity(i);
             finish();
         }
@@ -31,38 +39,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume(){
         super.onResume();
 
-        if(!isNetworkAvailable())
+        if(isNetworkAvailable())
         {
-            Intent i = new Intent(MainActivity.this, NoInternetActivity.class);
+            Intent i = new Intent(NoInternetActivity.this, MainActivity.class);
             startActivity(i);
             finish();
         }
     }
 
-    protected void onPause(){
-        super.onPause();
-    }
-
-    protected void onStop(){
-        super.onStop();
-    }
-
-    protected void onRestart(){
-        super.onRestart();
-
-        if(!isNetworkAvailable())
-        {
-            Intent i = new Intent(MainActivity.this, NoInternetActivity.class);
-            startActivity(i);
-            finish();
-        }
-    }
-
-    protected void onDestroy(){
-        super.onDestroy();
-    }
-
-    public boolean isNetworkAvailable() {
+    private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
---
+++ app/src/main/java/com/example/android/medikart/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.android.medikart;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.net.InetAddress;
+
+/**
+ * Created by Harsh on 05-03-2018.
+ */
+
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+
+            // Timer is used here to display logo of the company
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
